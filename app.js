5170bf8a24c8ccc6eba3f15f9575a817001115db
Devin Giannoni <>
2018-11-26 11:52:51 -0700

    /event get route is up and listening

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const db = require('./db');
+const cors = require('cors');
 const app = express();
 
+
 const port = process.env.PORT || 3000;
+const EventController = require('./controllers/EventController');
+
+app.use(cors());
+
+app.use('/event', EventController);
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
