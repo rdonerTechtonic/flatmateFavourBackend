c6869efdc4281cc609646c0af43f058e71342fda
Ryan Doner <>
2018-11-26 15:11:15 -0700

    added oath to app.js

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 const express = require('express');
 const app = express();
-
+const db = require('./db');
 const port = process.env.PORT || 3000;
+
+// server.js
+
+// const express = require('express');
+// const app = express();
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+// Authentication middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://YOUR_AUTH0_DOMAIN/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'YOUR_API_IDENTIFIER',
+  issuer: `https://YOUR_AUTH0_DOMAIN/`,
+  algorithms: ['RS256']
+});
+
+// This route doesn't need authentication
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+
+// This route need authentication
+app.get('/api/private', checkJwt, function(req, res) {
+  res.json({
+    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  });
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
