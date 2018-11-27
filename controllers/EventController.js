93e10233ea2f113bad0ba042f94650d9cd393a60
Devin Giannoni <>
2018-11-27 16:57:22 -0700

    Fixed a bug with the user creation.

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -7,28 +7,14 @@ router.use(bodyParser.json());
 
 
 router.get('/?', function (req, res) {
-  let searchParams = {}
-  Event.find(searchParams, (err,events) => {
+  Event.find({houseId: req.query.houseId}, (err,events) => {
     if (err) return res.status(500).send('Events not found.')
     res.status(200).send(events)
   })
 })
 
-// {
-// "eventTitle": "Mow the Lawn",
-// "eventOwner": "5bf5a3fa16018b9d0931b722",
-// "eventAssignees": ["5bf5a3fa16018b9d0931b721","5bf5a3fa16018b9d0931b723"],
-// "eventDescription": "It's not gonna mow itself",
-// "eventStartDate": "2018-01-01T00:00:00.000Z",
-// "eventEndDate": "2018-01-01T00:00:00.000Z",
-// "eventLocation": "Back Yard",
-// "eventStatus": "pending",
-// "houseId": "5bf5a3fa16018b9d0931b72b"
-// }
-
-//Above is the format of the object that is required by the function below.
 router.post('/?', function (req, res) {
-  Event.create(req.body.event, (err, event) => {
+  Event.create(req.body, (err, event) => {
     if (err) return res.status(500).send('Event not posted.')
     console.log(event);
     res.status(200).send(event);
@@ -36,6 +22,7 @@ router.post('/?', function (req, res) {
 })
 
 router.put('/?', function (req, res) {
+  console.log(req.body);
   Event.update({_id: req.query.eventId},
     {
       eventTitle: req.body.eventTitle,
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,28 +6,14 @@ const Roommate = require('../models/User');
 const router = express.Router();
 router.use(bodyParser.json());
 
-
-
-// let userObj = {
-//   "userName": "Daffy Duck",
-//   "userEmail": "daffy@gmail.com",
-//   "userPassword": "password123",
-//   "houseId": "5bf5a3fa16018b9d0931b72a",
-// }
-
-
-
-
 router.post('/?', function (req, res) {
-  Roommate.create(req.body.roommate, (err, roommate) => {
+  Roommate.create(req.body, (err, roommate) => {
     if (err) return res.status(500).send("There was a problem registering the user.");
     res.status(200).json(roommate);
   })
 });
 
-
 router.get('/?', function (req, res) {
-  console.log(req.query);
   Roommate.find(
     {houseId: req.query.houseId},
   // {houseID: mongoose.Types.ObjectId(req.query.houseId)},
